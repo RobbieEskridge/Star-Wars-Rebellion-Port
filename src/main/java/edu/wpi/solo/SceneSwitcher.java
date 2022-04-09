71e34f2c08a6758b33a2b2652f5ce1fc602d2b9b
RobbieEskridge <robceskridge@wpi.edu>
2022-04-09 15:52:55 -0400

    bot test

--- src/main/java/edu/wpi/solo/SceneSwitcher.java
+++ src/main/java/edu/wpi/solo/SceneSwitcher.java
@@ -1,12 +1,10 @@
-package edu.wpi.cs3733.c22.teamA;
+package edu.wpi.solo;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import edu.wpi.cs3733.c22.teamA.auth0.Auth0Login;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,27 +14,10 @@ public class SceneSwitcher {
   public static List<SCENES> fxmlval = new ArrayList<SCENES>();
 
   public enum SCENES {
-    LOGIN,
     HOME,
-    SELECT_SERVICE_REQUEST,
-    LAUNDRY_SR,
-    FOOD_DELIVERY_SR,
-    LANGUAGE_SR,
-    EQUIPMENT_DELIVERY_SR,
-    RELIGIOUS_SR,
-    SANITATION_SR,
-    FLORAL_DELIVERY_SR,
-    MEDICINE_DELIVERY_SR,
-    MAINTENANCE_SR,
-    GIFT_DELIVERY_SR,
-    SECURITY_SR,
-    CONSULTATION_SR,
-    DATA_VIEW,
     SETTINGS,
-    LOAD_BACKUP,
-    SAVE_BACKUP,
     MAP,
-    ABOUT;
+    CREDITS;
   }
 
   private final HashMap<SCENES, String> screenMap = new HashMap<>();
@@ -44,27 +25,10 @@ public class SceneSwitcher {
 
   public SceneSwitcher() {
 
-    addScene(SceneSwitcher.SCENES.LOGIN, "views/Login.fxml");
-    addScene(SceneSwitcher.SCENES.HOME, "views/Home.fxml");
-    addScene(SCENES.SETTINGS, "views/settings/Settings.fxml");
-    addScene(SCENES.LOAD_BACKUP, "views/settings/LoadBackup.fxml");
-    addScene(SCENES.SAVE_BACKUP, "views/settings/SaveBackup.fxml");
-    addScene(SCENES.ABOUT, "views/About.fxml");
-    addScene(SCENES.SELECT_SERVICE_REQUEST, "views/servicerequest/SelectServiceRequest.fxml");
-    addScene(SCENES.LAUNDRY_SR, "views/servicerequest/LaundrySR.fxml");
-    addScene(SCENES.FOOD_DELIVERY_SR, "views/servicerequest/FoodDeliverySR.fxml");
-    addScene(SCENES.LANGUAGE_SR, "views/servicerequest/LanguageSR.fxml");
-    addScene(SCENES.EQUIPMENT_DELIVERY_SR, "views/servicerequest/EquipmentDeliverySR.fxml");
-    addScene(SCENES.RELIGIOUS_SR, "views/servicerequest/ReligiousSR.fxml");
-    addScene(SCENES.SANITATION_SR, "views/servicerequest/SanitationSR.fxml");
-    addScene(SCENES.FLORAL_DELIVERY_SR, "views/servicerequest/FloralDeliverySR.fxml");
-    addScene(SCENES.MEDICINE_DELIVERY_SR, "views/servicerequest/MedicineDeliverySR.fxml");
-    addScene(SCENES.CONSULTATION_SR, "views/servicerequest/ConsultationSR.fxml");
-    addScene(SCENES.GIFT_DELIVERY_SR, "views/servicerequest/GiftDeliverySR.fxml");
-    addScene(SCENES.MAINTENANCE_SR, "views/servicerequest/MaintenanceSR.fxml");
-    addScene(SCENES.SECURITY_SR, "views/servicerequest/SecuritySR.fxml");
-    addScene(SCENES.DATA_VIEW, "views/DataView.fxml");
-    addScene(SCENES.MAP, "views/Map.fxml");
+    addScene(SCENES.HOME, "views/Home.fxml");
+    addScene(SCENES.SETTINGS, "views/Settings.fxml");
+    addScene(SCENES.CREDITS, "views/Credits.fxml");
+    // addScene(SCENES.MAP, "views/Map.fxml");
   }
 
   public void addScene(SCENES name, String pathToFXML) {
@@ -72,12 +36,6 @@ public class SceneSwitcher {
   }
 
   public void switchScene(SCENES name) throws IOException {
-
-    if (name == SCENES.LOGIN)
-    {
-      //Auth0Login.login();
-      //return;
-    }
 
     FXMLLoader loader = new FXMLLoader();
     URL xmlUrl = App.class.getResource(screenMap.get(name));
@@ -89,6 +47,5 @@ public class SceneSwitcher {
     App.getStage().setScene(currentScene);
     App.getStage().show();
     fxmlval.add(name);
-
   }
 }
